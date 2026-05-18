d4dac344036525e6c37014a6c344a800cd81754b
gilcris1980 <gilcrischatto@gmail.com>
2026-05-18 10:27:27 +0800

    Fixed login

--- js/login.js
+++ js/login.js
@@ -130,7 +130,7 @@ document.addEventListener('DOMContentLoaded', () => {
             submitButton.innerHTML = 'Logging in...';
 
             const response = await fetch(
-                'http://127.0.0.1:8000/api/login',
+                'https://chattoshop-api.onrender.com/api/login',
                 {
                     method: 'POST',
 
